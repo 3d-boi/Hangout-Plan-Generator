7955e36bd1171f965f91cc9c38aedc86afed2e89
Marwan Hany <marwanhany313@gmail.com>
2024-07-13 15:35:22 +0300

    some small changes

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ import email.message
 import smtplib
 
 location_list = ['School', 'Ahl-Misr Garden', 'Future Mall']
-will_generate = True#bool(random.getrandbits(1))
+will_generate = bool(random.getrandbits(1))
 
 dotenv.load_dotenv('.env')
 
 def generate_plan():
     location = random.choice(location_list)
     time = datetime.time(random.randint(6,9),00).strftime('%H:%M')
+    email_subject = 'SSS Hangout Invitation'
     email_msg = f"""
     Greetings, Esteemed Member,
 
@@ -30,15 +31,33 @@ def generate_plan():
 
     ~The SSS Planning Department~
     """
-    send_email(email_msg)
+    send_email(email_msg,email_subject)
 
-def send_email(message:str):
+def initiate_members():
+    email_subject = 'Exciting News: Introducing the SSS Planning Department'
+    email_msg = """
+    Greetings, Esteemed Members,
+
+    We are thrilled to announce the formation of the new Planning Department within the Secret Shit Society (SSS).
+    This elite team of master strategists is dedicated to crafting ingenious and thrilling plans for our society's future activities.
+
+    Expect more details about upcoming hangouts and events to be revealed soon. Prepare for a series of unforgettable experiences and adventures!
+
+    Stay tuned and keep your calendars flexible.
+
+    In shadows and secrecy,
+
+    ~The SSS Planning Department~
+    """
+    send_email(email_msg,email_subject)
+
+def send_email(message:str,subject:str):
     sender_email = str(os.environ.get('SENDER_EMAIL'))
     email_passkey = str(os.environ.get('EMAIL_PASSKEY'))
-    recipient_emails = [str(os.environ.get('RECIPIENT_EMAILS'))]
+    recipient_emails = [str(os.environ.get('RECIPIENT_EMAIL1')),str(os.environ.get('RECIPIENT_EMAIL2')),str(os.environ.get('RECIPIENT_EMAIL3'))]
     
     mail = email.message.Message()
-    mail['Subject'] = "SSS Hangout Invitation"
+    mail['Subject'] = subject
     mail.set_payload(message)
     mail_str = mail.as_string()
     
